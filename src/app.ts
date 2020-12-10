34fd70d99a1808fe0eb624451d428065d6998df3
Rory Claasen <rory@xbox.com>
2020-12-10 17:30:47 +0000

    Improves start script

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 const adventArgs = process.argv.slice(2);
 if (adventArgs.length !== 2) {
-    console.error('You must only pass in the year and the day of the month');
+    console.error('You may only pass in the year and the day of the month');
     process.exit(1);
 }
-const year = +adventArgs[0];
-const day = +adventArgs[1];
 
-if (isNaN(year) || isNaN(day)) {
-    console.error('Year and day of the month must be numbers');
-    process.exit(1);
-}
+const validate = (arg: string, errMessage: string, lowerBound: number, upperBound?: number) => {
+    const num = +arg;
+    if (isNaN(num) || num < lowerBound || (!!upperBound && num > upperBound)) {
+        console.error(errMessage);
+        process.exit(1);
+    }
+    return num;
+};
+
+const year = validate(adventArgs[0], 'Year must be a number greater than or equal to 2020', 2020);
+const day = validate(adventArgs[1], 'Day must be a number between 1 and 25', 1, 25);
 
 const file = `${year}/day${day}`;
 const importFile = import(file) as Promise<{ default: () => Promise<unknown[]> }>;
@@ -22,6 +27,10 @@ importFile
         });
     })
     .catch((err) => {
-        console.error(err);
+        if (err.code === 'MODULE_NOT_FOUND') {
+            console.error('No answer exists for this day');
+        } else {
+            console.error(err);
+        }
         process.exit(1);
     });
