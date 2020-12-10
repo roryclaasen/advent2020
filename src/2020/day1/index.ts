07a2e33e6f2f05fbc912efc1b34f4f8701786ba9
Rory Claasen <rory@xbox.com>
2020-12-10 14:07:56 +0000

    Finishes Day 1

--- src/2020/day1/index.ts
+++ src/2020/day1/index.ts
@@ -9,34 +9,54 @@ const readInput = async () => {
     return input.toString();
 };
 
-const findEntriesThatSum = (sum: number, entries: number[]) => {
-    let found = false;
-    let result: number[] = [0, 0];
-
-    for (let i = 0; i < entries.length; i++) {
-        if (found) break;
-        const first = entries[i];
-
-        for (let j = i + 1; j < entries.length; j++) {
-            const second = entries[j];
-
-            if (first + second === sum) {
-                result = [first, second];
-                found = true;
-                break;
-            }
-        }
-    }
-
+const product = (array: number[]) => {
+    let result = array[0];
+    array.slice(1).forEach((num) => (result *= num));
     return result;
 };
 
-async function main() {
+const sumArray = (array: number[]) => {
+    let result = array[0];
+    array.slice(1).forEach((num) => (result += num));
+    return result;
+};
+
+const findEntriesThatSum = (target: number, noEntries: number, entries: number[]) => {
+    let found = false;
+
+    const findResult = (startIndex: number, current: number[]): number[] => {
+        const level = current.length;
+        for (let i = startIndex; i < entries.length; i++) {
+            if (found) break;
+            current[level] = entries[i];
+
+            if (level + 1 === noEntries) {
+                found = sumArray(current) === target;
+                if (found) break;
+            } else {
+                current = findResult(i + 1, current);
+            }
+        }
+
+        if (!found) current.pop();
+        return current;
+    };
+
+    const result = findResult(0, []);
+
+    if (!found) throw new Error(`Could not find ${noEntries} numbers that summed ${target}`);
+    return result;
+};
+
+const main = async () => {
+    const target = 2020;
+
     const numbers = (await readInput()).split('\n').map((num) => +num);
-    const answer = findEntriesThatSum(2020, numbers);
-    return answer[0] * answer[1];
-}
+    const answerPart1 = product(findEntriesThatSum(target, 2, numbers));
+    const answerPart2 = product(findEntriesThatSum(target, 3, numbers));
+    return `Answer part 1: ${answerPart1}\nAnswer part 2: ${answerPart2}`;
+};
 
 main()
-    .then((result) => console.log(`Answer: ${result}`))
+    .then((result) => console.log(result))
     .catch(console.error);
